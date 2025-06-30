14f45eb06a2224609ea80e39ffbd8c2ef5ea970e
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-06-30 14:33:45 +0530

    c

--- app/Http/Controllers/Api/PratihariSebaApiController.php
+++ app/Http/Controllers/Api/PratihariSebaApiController.php
@@ -10,6 +10,7 @@ use App\Models\PratihariSebaBeddhaAssign;
 use App\Models\PratihariSebaManagement;
 use App\Models\PratihariSeba;
 use App\Models\PratihariSebaMaster;
+use App\Models\PratihariBeddhaMaster;
 
 use Carbon\Carbon;
 
