b94c4ad447ada257fe1b719c8afc6622ada2341e
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-07-10 16:41:46 +0530

    d

--- resources/views/partials/_user_card.blade.php
+++ resources/views/partials/_user_card.blade.php
@@ -2,8 +2,7 @@
     <div class="seba-user-card position-relative text-center">
         <span class="online-indicator" title="Active"></span>
         <a href="{{ route('admin.viewProfile', $user->pratihari_id) }}" class="text-decoration-none text-dark">
-            <img src="{{ $user->profile_photo ? asset($user->profile_photo) : asset('assets/img/brand/monk.png') }}"
-                class="user-avatar">
+            <img src="{{ $user->profile_photo ? asset($user->profile_photo) : asset('assets/img/brand/monk.png') }}" style="width: 60px" class="user-avatar">
         </a>
         <a href="{{ route('admin.viewProfile', $user->pratihari_id) }}" class="text-decoration-none text-dark">
             <div class="fw-semibold">{{ $user->first_name }} {{ $user->last_name }}</div>
