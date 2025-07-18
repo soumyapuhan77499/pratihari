4a28cac4b1b21e1070450325ae6e2ea5ca7bb020
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-07-18 12:08:24 +0530

    d

--- resources/views/admin/seba-calendar.blade.php
+++ resources/views/admin/seba-calendar.blade.php
@@ -249,4 +249,17 @@
 
     {{-- Select2 JS --}}
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <!-- Select2 CSS -->
+
+<!-- Select2 JS -->
+
+<script>
+    $(document).ready(function () {
+        $('.select2').select2({
+            placeholder: "Select Name",
+            allowClear: true,
+            width: '100%'
+        });
+    });
+</script>
 @endsection
