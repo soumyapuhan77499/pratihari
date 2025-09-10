cc59015ea071eb6f65627417d146a29dfdddd40d
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-09-10 16:08:00 +0530

    y

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,9 @@ use App\Http\Controllers\Api\PratihariOccupationApiController;
 use App\Http\Controllers\Api\PratihariSocialMediaApiController;
 use App\Http\Controllers\Api\PratihariSebaApiController;
 use App\Http\Controllers\Api\PratihariNoticeController;
-
 use App\Http\Controllers\Api\StatusController;
 
-Route::post('/send-otp', [OtpController::class, 'sendOtp'])->withoutMiddleware('auth');
+Route::match(['get', 'post'], '/send-otp', [OtpController::class, 'sendOtp'])->withoutMiddleware('auth');
 Route::post('/verify-otp', [OtpController::class, 'verifyOtp'])->name('admin.verifyOtp');
 
 Route::middleware('auth:sanctum')->post('/userLogout', [OtpController::class, 'userLogout']);
