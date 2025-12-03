2ccaa4e9082fbd4ddeb892a5e35eba2f3e9c172e
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-12-03 18:18:53 +0530

    f

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,17 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * For API (Sanctum) we do NOT redirect to any "login" route.
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): ?string
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        // Returning null tells Laravel not to try to redirect to route('login')
+        return null;
     }
 }
